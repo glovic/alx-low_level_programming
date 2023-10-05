50a87e79ffbacddbceac631f240373ba0e7bbb7c
Victor Nwaogu <xtineviktor@gmail.com>
2023-10-05 08:52:10 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function is the entry point of the program. It prints
+ *              a message to the standard output with double quotation marks.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
+
