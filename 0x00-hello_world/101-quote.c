d6f7e56e447ae928532bbb95b6b32eebfa0fcaea
Victor Nwaogu <xtineviktor@gmail.com>
2023-10-05 10:00:41 -0700

    UNIX is basically a simple operating system

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function is the entry point of the program. It prints
+ *              the specified message to the standard error.
+ *
+ * Return: Always 1 (error)
+ */
+int main(void)
+{
+    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+    return (1);
+}
