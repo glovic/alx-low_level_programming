6706ef3a24a01f1f4eb807636bc3a1b07caa5c72
Victor Nwaogu <xtineviktor@gmail.com>
2023-10-05 09:09:52 -0700

    various types on the computer

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,7 +9,7 @@
  * Return: Always 0 (success)
  */
 int main(void)
-}
+{
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
     	printf("Size of an int: %zu byte(s)\n", sizeof(int));
     	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
