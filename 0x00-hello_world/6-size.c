ae8f998d3d73566f50b3c0b24ffbc486e98ce0a1
Victor Nwaogu <xtineviktor@gmail.com>
2023-10-05 09:12:57 -0700

    various types on the computer

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,16 +4,17 @@
  * main - Entry point of the program
  *
  * Description: This function is the entry point of the program. It prints
- *              a message to the standard output with double quotation marks.
+ *              the sizes of various types to the standard output.
  *
  * Return: Always 0 (success)
  */
 int main(void)
 {
+	/* Use tabs for indentation */
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
-    	printf("Size of an int: %zu byte(s)\n", sizeof(int));
-    	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
-    	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
-    	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 	return (0);
 }
