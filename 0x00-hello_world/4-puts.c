fe01f69cf6c9ede9678cb8f0a5cb6135316c8145
Victor Nwaogu <xtineviktor@gmail.com>
2023-10-05 08:17:15 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,12 +4,12 @@
  * main - Entry point of the program
  *
  * Description: This function is the entry point of the program. It prints
- *              a message to the standard output.
+ *              a message to the standard output with double quotation marks.
  *
  * Return: Always 0 (success)
  */
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle");
-    return (0);
+    puts("\"Programming is like building a multilingual puzzle\"");
+    return 0;
 }
