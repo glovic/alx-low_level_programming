bae1a52f227ade05b6156036ffb9caf4049d9b03
Victor Nwaogu <xtineviktor@gmail.com>
2023-10-05 08:07:25 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,8 @@
 /*
  *C program that prints exactly Programming is like building a multilingual puzzle
  */
-
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle);
+	puts(Programming is like building a multilingual puzzle);
 	return (0);
 }	
