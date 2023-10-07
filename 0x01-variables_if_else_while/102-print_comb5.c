08ee86f245167acd5971d9e3bbbcf0af121fa121
Victor Nwaogu <xtineviktor@gmail.com>
2023-10-06 21:15:29 -0700

    program that prints all possible combinations of two two-digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,7 +8,7 @@
  *
  * Return: Always 0 (success)
  */
-int main (void)
+int main(void)
 {
 	int num1, num2;
 
@@ -29,7 +29,7 @@ int main (void)
 			putchar(' ');
 		}
 	}
-	
+
 	putchar('\n');
 
 	return (0);
