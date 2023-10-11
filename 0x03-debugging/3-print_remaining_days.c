940c1ccc7908473b7d91d295fe9c40e7264b957c
Victor Nwaogu <xtineviktor@gmail.com>
2023-10-11 12:43:49 -0700

    debugging 4

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -7,13 +7,12 @@
  * @month: month in number format
  * @day: day of month
  * @year: year
- *
  * Return: void
  */
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0 || year % 400 == 0))
+	if ((year % 4 == 0 && year % 100 == 0) && !(year % 400 == 0))
 	{
 		if (month >= 2 && day >= 60)
 		{
@@ -21,7 +20,7 @@ void print_remaining_days(int month, int day, int year)
 		}
 
 		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 365 - day);
+		printf("Remaining days: %d\n", 366 - day);
 	}
 	else
 	{
