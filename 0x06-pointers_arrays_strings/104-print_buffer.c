8df63ac571aa717aa46797b7b1efdf4d7b6551cc
Victor Nwaogu <xtineviktor@gmail.com>
2023-10-19 12:56:49 -0700

    a function that prints a buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,46 +2,51 @@
 #include <stdio.h>
 
 /**
- * print_buffer - Prints the content of a buffer in a specific format.
+ * print_buffer - Prints a buffer in a structured format.
  * @b: The buffer to be printed.
- * @size: The number of bytes to print.
+ * @size: The number of bytes to be printed from the buffer.
+ *
+ * Return: No return value.
  */
 void print_buffer(char *b, int size)
 {
-	int i, j;
+	int byte, index;
 
-	for (i = 0; i < size; i += 10)
+	for (byte = 0; byte < size; byte += 10)
 	{
-		printf("%08x: ", i);
-	}
-	for (j = i; j < i + 10; j++)
-	{
-		if (j < size)
+		printf("%08x: ", byte);
+
+		for (index = 0; index < 10; index++)
 		{
-			printf("%02x", b[j]);
-		}
-		else
-		{
-			printf("  ");
-		}
-		if (j % 2 == 1)
-		{
-			printf(" ");
-		}
-	}
-	for (j = i; j < i + 10; j++)
-	{
-		if (j < size)
-		{
-			if (b[j] >= 32 && b[j] <= 126)
-			{
-				printf("%c", b[j]);
-			}
+			if ((index + byte) >= size)
+				printf("  ");
+
 			else
-			{
-				printf(".");
-			}
+				printf("%02x", *(b + index + byte));
+
+			if ((index % 2) != 0 && index != 0)
+				printf(" ");
 		}
+
+		for (index = 0; index < 10; index++)
+		{
+			if ((index + byte) >= size)
+				break;
+
+			else if (*(b + index + byte) >= 31 &&
+				 *(b + index + byte) <= 126)
+				printf("%c", *(b + index + byte));
+
+			else
+				printf(".");
+		}
+
+		if (byte >= size)
+			continue;
+
+		printf("\n");
 	}
-	printf("\n");
+
+	if (size <= 0)
+		printf("\n");
 }
