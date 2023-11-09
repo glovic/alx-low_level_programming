caeb6a9ac4b16423e466a2611cdfc60dab1ba262
Victor Nwaogu <xtineviktor@gmail.com>
2023-11-09 07:52:41 -0800

    a function that returns the sum of all its parameters

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - The sum of all it's parameters.
+ * @n: Number of passed parameters.
+ *
+ * Return: if n == 0 - 0.
+ *	Otherwise - sum of all parameters.
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list digits;
+	int sum = 0;
+	unsigned int idx;
+
+	va_start(digits, n);
+
+	if (n != 0)
+	{
+		for (idx = 0; idx < n; idx++)
+			sum += va_arg(digits, int);
+	}
+
+	va_end(digits);
+
+	return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+Readme file for variadic functions on C-programming
